0194d3a194b7f7b7ea8a157f242c793dfc70ca20
John Doty <john@d0ty.me>
2016-03-24 21:42:37 -0700

    Feed fetching for reals

--- sociallists/app.py
+++ sociallists/app.py
@@ -3,18 +3,23 @@ import json
 
 from flask import Flask, render_template
 from sociallists.river import feed_to_river
-from sociallists import db
+from sociallists import db, river
 
 app = Flask('sociallists')
 
-@app.route("/api/v1/river/<id>")
-def get_river(id):
-    feed = feedparser.parse("http://davepeck.org/feed/")
+@app.route("/api/v1/river/<user>/<id>")
+def get_river(user,id):
+    db_river = db.load_river_by_name(user,id)
+    feed_updates = []
+    if river:
+        updates = [
+            update for feed in db_river.feeds for update in feed.updates
+        ]
+        updates.sort(reverse=True, key=lambda u: u.update_time)
+        feed_updates = [ json.loads(u.data) for u in updates ]
 
-    river = feed_to_river(feed, 0)
-
-    # TODO: jsonp serialize
-    result = "onGetRiverStream("+json.dumps(river, indent=2, sort_keys=True)+");"
+    r = river.wrap_feed_updates(feed_updates)
+    result = "onGetRiverStream("+json.dumps(r, indent=2, sort_keys=True)+");"
     return (result, 200, {'content-type': 'application/javascript'})
 
 @app.route("/api/v1/feed/<id>")
