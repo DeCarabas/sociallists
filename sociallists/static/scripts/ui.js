6650ebf7b27907794d9d20620383503233cb07f1
John Doty <john@d0ty.me>
2016-04-10 08:01:13 -0700

    Components are functions

--- sociallists/static/scripts/ui.js
+++ sociallists/static/scripts/ui.js
@@ -1,69 +1,61 @@
-var React = require('react');
+var React = require('react'); // N.B. Still need this because JSX.
 
-export class RiverItem extends React.Component {
-  render() {
-    let style = {
-      border: "1px solid AAA",
-    };
+export const RiverItem = ({item}) => {
+  let style = {
+    border: "1px solid AAA",
+  };
 
-    return(
-      <div style={style}>
-        <a href={ this.props.item.link }>
-          <h4>{ this.props.item.title }</h4>
-        </a>
-        <p>{ this.props.item.body }</p>
-      </div>
-    );
-  }
-}
+  return(
+    <div style={style}>
+      <a href={ item.link }>
+        <h4>{ item.title }</h4>
+      </a>
+      <p>{ item.body }</p>
+    </div>
+  );
+};
 
-export class RiverFeedUpdate extends React.Component {
-  render() {
-    let style = {
-      margin: 3,
-    };
+export const RiverFeedUpdate = ({update}) => {
+  let style = {
+    margin: 3,
+  };
 
-    let update = this.props.update;
-    let udkeypart = update.feedUrl + '|' + update.whenLastUpdate;
-    let items = update.item;
-    return(
-      <div style={style}>
-        <h3>{update.feedTitle}</h3>
-        {
-          items.map(i =>
-            <RiverItem
-              item={i}
-              key={ udkeypart + '|' + i.id }
+  let udkeypart = update.feedUrl + '|' + update.whenLastUpdate;
+  let items = update.item;
+  return(
+    <div style={style}>
+      <h3>{update.feedTitle}</h3>
+      {
+        items.map(i =>
+          <RiverItem
+            item={i}
+            key={ udkeypart + '|' + i.id }
+          />
+        )
+      }
+    </div>
+  );
+};
+
+export const RiverColumn = ({river}) => {
+  let style = {
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "black",
+    width: 400,
+  };
+
+  let updates = river.updatedFeeds.updatedFeed;
+  return (
+    <div style={style}>
+      {
+        updates.map(u =>
+          <RiverFeedUpdate
+            update={u}
+            key={ u.feedUrl + '|' + u.whenLastUpdate }
             />
-          )
-        }
-      </div>
-    );
-  }
-}
-
-export class RiverColumn extends React.Component {
-  render() {
-    let style = {
-      borderWidth: 1,
-      borderStyle: "solid",
-      borderColor: "black",
-      width: 400,
-    };
-
-    let river = this.props.river;
-    let updates = river.updatedFeeds.updatedFeed;
-    return (
-      <div style={style}>
-        {
-          updates.map(u =>
-            <RiverFeedUpdate
-              update={u}
-              key={ u.feedUrl + '|' + u.whenLastUpdate }
-              />
-          )
-        }
-      </div>
-    );
-  }
-}
+        )
+      }
+    </div>
+  );
+};
