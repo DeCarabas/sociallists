33bb08eae19afbb843b194fe9b81a1cfa3efeadd
John Doty <john@d0ty.me>
2016-04-16 08:02:47 -0700

    Fetch blobs from the DB

--- sociallists/app.py
+++ sociallists/app.py
@@ -1,7 +1,7 @@
 import feedparser
 import json
 
-from flask import Flask, g, render_template, url_for
+from flask import abort, Flask, g, render_template, request, url_for
 from sociallists.river import feed_to_river
 from sociallists import db, river
 
@@ -39,6 +39,20 @@ def get_public_river(user,id):
     r = river.aggregate_river(user,id)
     result = "onGetRiverStream("+json.dumps(r, indent=2, sort_keys=True)+");"
     return (result, 200, {'content-type': 'application/javascript'})
+
+@app.route("/blob/<hash>")
+def get_blob(hash):
+    # Check the etag here; if it matches the hash then you've already got it
+    if request.if_none_match == hash:
+        abort(304)
+    with db.session() as db_session:
+        blob = db.get_blob(db_session, hash)
+        if blob is None:
+            abort(404)
+        return (blob.data, 200, {
+            'content-type': blob.contentType,
+            'ETag': hash,
+        })
 
 @app.teardown_request
 def shutdown_session(exception=None):
