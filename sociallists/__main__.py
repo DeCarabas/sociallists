e1d12a6820fd37a88c8005ec2e531e792ed6a8a8
John Doty <john@d0ty.me>
2016-04-29 13:28:53 -0700

    Refresh everything web request

--- sociallists/__main__.py
+++ sociallists/__main__.py
@@ -1,4 +1,12 @@
+import logging
+
 from sociallists.app import app
+
+level = logging.INFO
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    level=level,
+)
 
 # TODO: Config
 app.run(debug=True)
--- sociallists/app.py
+++ sociallists/app.py
@@ -1,7 +1,8 @@
 import feedparser
 import json
+import threading
 
-from flask import abort, Flask, g, render_template, request, url_for
+from flask import abort, Flask, g, render_template, request, url_for, Response
 from sociallists.river import feed_to_river
 from sociallists import db, feed, river
 
@@ -66,6 +67,34 @@ def get_public_river(user,id):
     r = rewrite_river(river.aggregate_river(user,id))
     result = "onGetRiverStream("+json.dumps(r, indent=2, sort_keys=True)+");"
     return (result, 200, {'content-type': 'application/javascript'})
+
+@app.route("/api/v1/river/<user>/refresh_all", methods=['POST'])
+def post_refresh_rivers(user):
+    with db.session() as db_session:
+        all_feeds = db.load_all_feeds(db_session)
+    done_feeds = []
+    done_feeds_condition = threading.Condition()
+
+    def update_progress(feed):
+        with done_feeds_condition:
+            done_feeds.append(feed)
+            done_feeds_condition.notify()
+
+    def progress_generator():
+        with done_feeds_condition:
+            while len(done_feeds) != len(all_feeds):
+                pct = int(100 * len(done_feeds) / len(all_feeds))
+                yield str(pct)+'\n'
+                done_feeds_condition.wait()
+        yield '100\n'
+
+    def update_main_thread():
+        batch = feed.FeedUpdateBatch()
+        batch.update_feeds(all_feeds, sync=False, done_callback=update_progress)
+
+    threading.Thread(target=update_main_thread).start()
+    return Response(progress_generator(), mimetype='text/plain')
+
 
 @app.route("/blob/<hash>")
 def get_blob(hash):
