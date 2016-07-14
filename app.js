00bb519de034c8b4f2209d09c245f3a92f1d7a12
John Doty <john@d0ty.me>
2016-07-14 15:52:10 -0700

    App launches the server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const electron = require('electron')
+const electron = require('electron');
+const child_process = require('child_process');
 
 // Module to control application life.
 const app = electron.app;
@@ -7,26 +8,54 @@ const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow = null;
+let python_server = null;
+
+console.log('Hello world!');
 
 function startPythonServer() {
   // Here we go.
+  if (python_server === null) {
+    const venv = `${__dirname}/venv`;
+    python_server = child_process.spawn(
+      'venv/bin/python',
+      ['-m', 'sociallists'],
+      {
+        cwd: __dirname,
+        env: {
+          VIRTUAL_ENV: venv,
+          DB_CONNECTION_STRING: "postgresql:///sociallists",
+          PYTHONPATH: __dirname,
+        },
+      });
+
+    python_server.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+
+    python_server.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
+
+    python_server.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+  }
 }
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  if (mainWindow === null) {
+    startPythonServer();
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/ui/index.html`);
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  })
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL(`file://${__dirname}/ui/index.html`);
+    mainWindow.on('closed', function () {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      mainWindow = null;
+    });
+  }
 }
 
 // This method will be called when Electron has finished
